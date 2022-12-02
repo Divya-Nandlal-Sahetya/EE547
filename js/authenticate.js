2e3e1075eeba584bd60770f54dcefa1d4134386e
AkshataJ96 <ajahagir@usc.edu>
2022-12-02 02:25:38 -0800

    gmail + passport + jwt

--- js/authenticate.js
+++ js/authenticate.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-
+const session = require('express-session');
 const jwt = require('jsonwebtoken')
 const passport = require('passport')
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+var Gmail = require('node-gmail-api')
+var successAccessToken = null
+var successRefreshToken = null
 
 const DATA = [ // should be a database or something persistant
     {email:"team12ee547@gmail.com", provider:"google"} // user data from OAuth has no password
@@ -22,9 +25,31 @@ const config = {
 }
 
 // Middlewares
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "mysecret"
+  }))
 app.use(bodyParser.urlencoded({ extended: false })) 
 app.use(cookieParser())
 app.use(passport.initialize()); 
+app.use(passport.session());
+
+async function listLabels(auth) {
+    const gmail = google.gmail({version: 'v1', auth});
+    const res = await gmail.users.labels.list({
+      userId: 'me',
+    });
+    const labels = res.data.labels;
+    if (!labels || labels.length === 0) {
+      console.log('No labels found.');
+      return;
+    }
+    console.log('Labels:');
+    labels.forEach((label) => {
+      console.log(`- ${label.name}`);
+    });
+}
 
 // Utility functions for checking if a user exists in the DATA array - Note: DATA array is flushed after every restart of server
 function FindOrCreate(user){
@@ -71,12 +96,14 @@ passport.use(new GoogleStrategy({
     callbackURL: "http://localhost:8080/handleGoogleRedirect"
   },
   function(accessToken, refreshToken, profile, done) {
+    successAccessToken = accessToken;
+    successRefreshToken = refreshToken;
       console.log(accessToken, refreshToken, profile)
       console.log("GOOGLE BASED OAUTH VALIDATION GETTING CALLED")
       return done(null, profile)
   }
 ))
-  
+
 // These functions are required for getting data To/from JSON returned from Providers
 passport.serializeUser(function(user, done) {
     console.log('I should have jack ')
@@ -87,6 +114,13 @@ passport.deserializeUser(function(obj, done) {
     done(null, obj)
 })
 
+const isLoggedIn = (req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+}
 
 app.get('/', (req, res)=>{
     res.sendFile('home.html', {root: __dirname})
@@ -95,26 +129,60 @@ app.get('/login', (req, res)=>{
     res.sendFile('login.html', {root: __dirname})
 })
 
+app.get("/failed", (req, res) => {
+    res.send("Failed")
+})
+app.get("/success",isLoggedIn, async (req, res) => {
+    console.log("************req***********",successAccessToken);
+    const gmail = new Gmail(successAccessToken);
+    s = gmail.messages('label:inbox', {max: 10})
+    var i =0;
+    s.on('data', function (d) {
+    //console.log(i + " " + d.snippet)
+    i = i+1;
+    })
+
+    res.send(`Welcome ${req.user.displayName}`);
+})
+
+app.get("/successGetEmail",isLoggedIn, async (req, res) => {
+    console.log("************req***********",successAccessToken);
+    const gmail = new Gmail(successAccessToken);
+    s = gmail.messages('label:inbox', {max: 10})
+    var i =0;
+    var first = "";
+    s.on('data', function (d) {
+        if(i==0){
+            first = d.snippet;
+            console.log("first: " + first)
+        }
+    //console.log(i + " " + d.snippet)
+    i = i+1;
+    })
+
+    res.send("First" + first);
+})
+
 // OAuth Authentication, Just going to this URL will open OAuth screens
-app.get('/auth/google',  passport.authenticate('google', { scope: ['profile','email'] }))
+app.get('/auth/google',  passport.authenticate('google', { scope: ['profile','https://mail.google.com/',
+//'https://www.googleapis.com/auth/gmail.metadata',
+'https://www.googleapis.com/auth/gmail.modify',
+'https://www.googleapis.com/auth/gmail.readonly',] }))
 
 // Oauth user data comes to these redirectURLs
-app.get('/googleRedirect', passport.authenticate('google'),(req, res)=>{
-    console.log('redirected', req.user)
-    let user = {
-        displayName: req.user.displayName,
-        name: req.user.name.givenName,
-        email: req.user._json.email,
-        provider: req.user.provider }
-    console.log(user)
+app.get('/handleGoogleRedirect', passport.authenticate('google', {
+    failureRedirect: '/failed',
+    successRedirect: '/success'
+}))
 
-    FindOrCreate(user)
-    let token = jwt.sign({
-        data: user
-        }, 'secret', { expiresIn: 60 }); // expiry in seconds
-    res.cookie('jwt', token)
-    res.redirect('/')
-})
+app.get('/getEmail', passport.authenticate('google', {
+    failureRedirect: '/failed',
+    successRedirect: '/successGetEmail'
+}))
+app.get('/getCalendar', passport.authenticate('google', {
+    failureRedirect: '/failed',
+    successRedirect: '/successGetEmail'
+}))
 
 // This url will only open, if the user is signed in
 app.get('/profile', passport.authenticate('jwt', { session: false }) ,(req,res)=>{
